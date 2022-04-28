225c8d12fec3ac4396c87056fc286f6eb5485057
Anthony Herrera <antherrera@csumb.edu>
2022-04-28 15:14:56 -0700

    Login/Registration functionalities. Needs further testing and adjustments for web api.

--- app/src/main/java/com/example/notetaker/LoginActivity.java
+++ app/src/main/java/com/example/notetaker/LoginActivity.java
@@ -2,13 +2,115 @@ package com.example.notetaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.notetaker.interfaces.LoginInterface;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText username, password;
+
+    private Button loginBtn;
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        loginBtn = (Button) findViewById(R.id.login);
+
+        register = (TextView) findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                LoginActivity.this.finish();
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser() {
+        final String uname = username.getText().toString().trim();
+        final String pass = password.getText().toString().trim();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(LoginInterface.url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        LoginInterface api = retrofit.create(LoginInterface.class);
+
+        Call<String> call = api.getUserLogin(uname, pass);
+
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                Log.i("ResponseString", response.body().toString());
+
+                if(response.isSuccessful()) {
+                    if(response.body() != null) {
+                        Log.i("onSuccess", response.body().toString());
+
+                        String jsonResponse = response.body().toString();
+
+                        parseLoginData(jsonResponse);
+                    } else {
+                        Log.i("onEmptyResponse", "Returned empty");
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void parseLoginData(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            if(jsonObject.getString("status").equals("true")) {
+
+                Toast.makeText(LoginActivity.this, "Login Success!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, Home.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                this.finish();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/notetaker/RegisterActivity.java
+++ app/src/main/java/com/example/notetaker/RegisterActivity.java
@@ -2,13 +2,115 @@ package com.example.notetaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.notetaker.interfaces.RegisterInterface;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText username, password, rePassword;
+
+    private Button registerBtn;
+
+    private TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        rePassword = (EditText) findViewById(R.id.repassword);
+
+        registerBtn = (Button) findViewById(R.id.register);
+
+        login = (TextView) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                RegisterActivity.this.finish();
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+    }
+
+    private void registerUser() {
+        final String uname = username.getText().toString();
+        final String pass = password.getText().toString();
+        final String repass = rePassword.getText().toString();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(RegisterInterface.url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RegisterInterface api = retrofit.create(RegisterInterface.class);
+
+        Call<String> call = api.getUserRegi(uname, pass);
+
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                Log.i("ResponseString", response.body().toString());
+
+                if(response.isSuccessful()) {
+                    Log.i("onSuccess", response.body().toString());
+
+                    String jsonResponse = response.body().toString();
+                    try {
+                        parseRegData(jsonResponse);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Log.i("onEmptyResponse", "Returned empty");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void parseRegData(String response) throws JSONException {
+        JSONObject jsonObject = new JSONObject(response);
+
+        if(jsonObject.optString("status").equals("true")) {
+            Toast.makeText(RegisterActivity.this, "Registered Successfully!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(RegisterActivity.this, Home.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            this.finish();
+        } else {
+            Toast.makeText(RegisterActivity.this, jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
+        }
     }
 }
