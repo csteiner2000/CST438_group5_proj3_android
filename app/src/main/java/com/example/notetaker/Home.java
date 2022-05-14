a67e9bb116666842aab93ef570c6a3cfe1440f1f
Alejandro Ramirez <aeramirez258@gmail.com>
2022-05-14 14:07:33 -0700

    add hard coded api fetching of user notes

--- app/src/main/java/com/example/notetaker/Home.java
+++ app/src/main/java/com/example/notetaker/Home.java
@@ -6,12 +6,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.notetaker.database.RetrofitClientInstance;
+import com.example.notetaker.database.api.ApiEndpointInterface;
+import com.example.notetaker.database.models.User;
 import com.example.notetaker.noterecycler.NoteAdapter;
 import com.example.notetaker.noterecycler.NoteData;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Home extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -23,26 +31,33 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        List<NoteData> list = getData();
 
-        listener = new ClickListener();
-        adapter = new NoteAdapter(list, getApplicationContext(), listener);
+        ApiEndpointInterface api = RetrofitClientInstance.getRetrofitInstance().create(ApiEndpointInterface.class);
+        Call<User.ApiResponse> call = api.getUser("1", "1");
+        Call<List<NoteData>> call1 = api.getAllNotes(1);
+        call1.enqueue(new Callback<List<NoteData>>() {
+            @Override
+            public void onResponse(Call<List<NoteData>> call, Response<List<NoteData>> response) {
+                List<NoteData> list = response.body();
+                if (list == null) {
+                    Log.d("Data_response", "onFailure: " + call);
+                    Log.d("Data_response", "onFailure: could not parse response as list of notes");
+                    return;
+                }
+                Log.d("Data_response", "onResponse: successfully got " + list.size() + " notes");
+                listener = new ClickListener();
+                adapter = new NoteAdapter(list, getApplicationContext(), listener);
+                recyclerView = findViewById(R.id.recyclerView);
+                recyclerView.setAdapter(adapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(Home.this));
+            }
 
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(Home.this));
-    }
-
-    // Sample data for RecyclerView
-    private List<NoteData> getData() {
-        List<NoteData> list = new ArrayList<>();
-//        list.add(new NoteData("First Note", "May 23, 2015"));
-//        list.add(new NoteData("Second Note", "June 09, 2015"));
-//        list.add(new NoteData("Third Note", "April 27, 2017"));
-        list.add(new NoteData("First Note", "May 23, 2015", "This is the text for the first note"));
-        list.add(new NoteData("Second Note", "June 09, 2015", " text and more text, nice"));
-        list.add(new NoteData("Third Note", "April 27, 2017", "example"));
-        return list;
+            @Override
+            public void onFailure(Call<List<NoteData>> call, Throwable t) {
+                Log.d("Data_response", "onFailure: " + call);
+                Log.d("Data_response", "onFailure: failed to get an api response");
+            }
+        });
     }
 
     public class ClickListener {
@@ -53,3 +68,4 @@ public class Home extends AppCompatActivity {
         }
     }
 }
+
