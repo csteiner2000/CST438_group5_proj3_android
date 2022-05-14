ea45492847e9727ef3e4354627ab89cc31f84eff
Alejandro Ramirez <aeramirez258@gmail.com>
2022-05-13 22:12:23 -0700

    Add api endpoint for getting all user notes

--- app/src/main/java/com/example/notetaker/database/api/ApiEndpointInterface.java
+++ app/src/main/java/com/example/notetaker/database/api/ApiEndpointInterface.java
@@ -1,7 +1,9 @@
 package com.example.notetaker.database.api;
 
 import com.example.notetaker.database.models.User;
+import com.example.notetaker.noterecycler.NoteData;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -17,4 +19,7 @@ public interface ApiEndpointInterface {
             @Query("uName") String username,
             @Query("pWord") String password
     );
+
+    @GET("https://arcane-crag-19565.herokuapp.com/api/note/all")
+    Call<List<NoteData>> getAllNotes(@Query("userId") int userId);
 }
