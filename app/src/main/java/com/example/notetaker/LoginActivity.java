5fdd8d13ef0f5bca45062fbb3d9eaf7cf5b7b7d2
Alejandro Ramirez <aeramirez258@gmail.com>
2022-05-15 12:03:29 -0700

    Add proper logging in with api and shared preferences.
    Not so much room but hey, whatever its progress

--- app/src/main/java/com/example/notetaker/LoginActivity.java
+++ app/src/main/java/com/example/notetaker/LoginActivity.java
@@ -1,7 +1,5 @@
 package com.example.notetaker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,17 +9,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.notetaker.interfaces.LoginInterface;
+import androidx.appcompat.app.AppCompatActivity;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.example.notetaker.database.RetrofitClientInstance;
+import com.example.notetaker.database.api.ApiEndpointInterface;
+import com.example.notetaker.database.models.LoginResponse;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -38,9 +34,7 @@ public class LoginActivity extends AppCompatActivity {
 
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
-
         loginBtn = (Button) findViewById(R.id.login);
-
         register = (TextView) findViewById(R.id.register);
 
         register.setOnClickListener(new View.OnClickListener() {
@@ -51,7 +45,6 @@ public class LoginActivity extends AppCompatActivity {
                 LoginActivity.this.finish();
             }
         });
-
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,53 +58,43 @@ public class LoginActivity extends AppCompatActivity {
         final String uname = username.getText().toString().trim();
         final String pass = password.getText().toString().trim();
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(LoginInterface.url)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        if (uname.equals("") || pass.equals("")) {
+            // TODO: add error message so you can't try logging in w/out username or password
+        }
 
-        LoginInterface api = retrofit.create(LoginInterface.class);
-
-        Call<String> call = api.getUserLogin(uname, pass);
-
-        call.enqueue(new Callback<String>() {
+        ApiEndpointInterface api = RetrofitClientInstance.getRetrofitInstance().create(ApiEndpointInterface.class);
+        Call<LoginResponse> call = api.getUser(uname, pass);
+        call.enqueue(new Callback<LoginResponse>() {
             @Override
-            public void onResponse(Call<String> call, Response<String> response) {
-                Log.i("ResponseString", response.body().toString());
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                LoginResponse response1 = response.body();
+                if (response1 == null) {
+                    // TODO: add error message to log in screen
+                    return;
+                }
 
-                if(response.isSuccessful()) {
-                    if(response.body() != null) {
-                        Log.i("onSuccess", response.body().toString());
+                if (response1.success) {
+                    Toast.makeText(LoginActivity.this, "Login Success!", Toast.LENGTH_SHORT).show();
+                    Log.d("Data_response", "onResponse: login successful");
+                    Log.d("Data_response", "onResponse userId: " + response1.userId);
 
-                        String jsonResponse = response.body().toString();
+                    // save userId in shared preferences
+                    getSharedPreferences("noteTaker", MODE_PRIVATE).edit().putInt("userId", response1.userId).apply();
 
-                        parseLoginData(jsonResponse);
-                    } else {
-                        Log.i("onEmptyResponse", "Returned empty");
-                    }
+                    Intent intent = new Intent(LoginActivity.this, Home.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                } else {
+                    Log.d("Data_response", "onResponse: login failed");
+                    /// TODO: Display error message on login screen
                 }
             }
 
             @Override
-            public void onFailure(Call<String> call, Throwable t) {
-
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                Log.d("Data_response", "onFailure: " + call);
+                Log.d("Data_response", "onFailure: failed to get a response");
             }
         });
-    }
-
-    private void parseLoginData(String response) {
-        try {
-            JSONObject jsonObject = new JSONObject(response);
-            if(jsonObject.getString("status").equals("true")) {
-
-                Toast.makeText(LoginActivity.this, "Login Success!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(LoginActivity.this, Home.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                this.finish();
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 }
